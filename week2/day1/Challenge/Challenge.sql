cb21f0c18eee3169938eec9c1ddba670bbaae57a
FEL <fatima.ezzahra.lahouani00@gmail.com>
2025-10-10 01:07:18 +0100

    Add placeholder data for new actor entries in Challenge.sql

--- week2/day1/Challenge/Challenge.sql
+++ week2/day1/Challenge/Challenge.sql
@@ -10,4 +10,10 @@ FROM actor;
 INSERT INTO actor (first_name, last_name) 
 VALUES ('', '');
 -- output 1 row inserted because of the default values 
+bghyty t3rfy elax andrbk
+matsbix 
+ant333333sssssssssssssbbbbb
+lkhra
+maxi so9k 
+ayhayh dsrty yalh lah meak tal ghda inx2llah 
 
